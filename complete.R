0c62b55505d50216b57e362bb5a95f088c975fe9
Robert Stewart <robert.ian.stewart@gmail.com>
2014-12-13 22:01:28 -0600

    Split functions into separate files

---
+++ complete.R
@@ -0,0 +1,13 @@
+complete <- function(directory, id = 1:332) {
+    output <- data.frame(id = vector('integer'), nobs = vector('integer'))
+    
+    for (i in id) {
+        
+        fname <- paste("./", directory, sprintf("/%03d.csv",i), sep="")
+        mon_data <- read.csv(fname, header=TRUE, sep = ",")
+        
+        num_complete = sum(complete.cases(mon_data))
+        output <- rbind (output, data.frame(id = c(i), nobs = c(num_complete)))
+    }
+    return(output)
+}
---
+++ pollutantmean.R
@@ -0,0 +1,14 @@
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+
+    all_data <- vector('numeric')    
+    
+    for (i in id) {
+    
+        fname <- paste("./", directory, sprintf("/%03d.csv",i), sep="")
+        mon_data <- read.csv(fname, header=TRUE, sep = ",")
+        one_col <- mon_data[[pollutant]]
+        all_data <- append(all_data, one_col)
+    }
+    
+    return(mean(all_data,na.rm=TRUE))
+}
